e77059b06676e618f47aaa648f65dec816ff725a
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-23 18:11:01 +0530

    XAMARIN-34083 - sample added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Model/BookInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class BookInfo : Realms.RealmObject
+    {
+        #region Constructor
+
+        public BookInfo()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+
+        public string BookName { get; set; }
+
+        public string BookDescription { get; set; }
+
+        public string BookAuthor { get; set; }
+        #endregion
+    }
+}
